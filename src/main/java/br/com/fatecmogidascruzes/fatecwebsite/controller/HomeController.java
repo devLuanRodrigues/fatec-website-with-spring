a7a1743722617373a36831e0524135855b2f909c
Henrique Lopes <henriquebonfanti2018@gmail.com>
2024-06-17 11:17:48 -0300

    feat: cria pag Admin

--- src/main/java/br/com/fatecmogidascruzes/fatecwebsite/controller/HomeController.java
+++ src/main/java/br/com/fatecmogidascruzes/fatecwebsite/controller/HomeController.java
@@ -2,21 +2,25 @@ package br.com.fatecmogidascruzes.fatecwebsite.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping
-    public String home(Model model) {
+    public ModelAndView home(ModelAndView model) {
         logger.info("Accessing the home page");
 
-        return "index";
+        // add index.html to model and return the view without using 'return "index"'
+        model.setViewName("index");
+
+        return model;
     }
 }
